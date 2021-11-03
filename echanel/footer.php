7219e65dfbb91adc375a87234aaa8904b38cc944
harsha-ys <68203453+harsha-ys@users.noreply.github.com>
2021-11-03 21:11:18 +0800

    first page created

---
+++ echanel/footer.php
@@ -0,0 +1,33 @@
+   <!-- footer section --> 
+			<footer>
+				<div class="col-md-12 fsection">
+					<div class="col-md-4" id="contact">
+					<p>Category</p> <hr>
+					<ul>
+						<li><a href="signin.php">Search for Doctors</a></li>
+						<li><a href="signin.php">Contact with Doctors</a></li>
+					</ul>
+					</div>
+					<div class="col-md-4">
+						<p>Contact</p> <hr>
+						<ul>
+							<li>Yoshith Harshana</li>
+							<li>Email: abcdmail@gmail.com</li>
+							<li>Mobile: 077 1258623</li>
+						</ul>
+							
+					</div>
+					<div class="col-md-4 share_img">
+					<p>Link With</p> <hr>
+						<a href=""><img src="img/gmail.png" alt="gmail" style="margin-left:20px; height:40px; width:40px;"></a>
+						<a href=""><img src="img/twitter.png" alt="twitter" style="margin-left:20px;"></a>
+						<a href="" ><img src="img/fb-free.png" alt="facebok" style="margin-left:20px;"></a>
+
+						
+					</div>
+				</div>
+				
+
+			</footer>
+
+		<!-- footer section Ends--> 
