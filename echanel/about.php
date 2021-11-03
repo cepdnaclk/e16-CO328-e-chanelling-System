e47e90ec375aba5326c89123683e007719a98727
harsha-ys <68203453+harsha-ys@users.noreply.github.com>
2021-11-03 21:02:02 +0800

    created about.php

---
+++ echanel/about.php
@@ -0,0 +1,52 @@
+<?php include('header.php'); ?>
+
+
+
+
+
+	<!-- this is for donor registraton -->
+	<div class="main_content">
+		<h3 class="text-center">About Us</h3>
+		<div class="col-md-12" style="margin-top:30px; margin-bottom:30px;">
+			<div class="col-md-6">
+				<img src="img/1.jpg" alt="" class="img-responsive">
+			</div>
+
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:dodgerblue;">We are Beside you</h3>
+					<h3 style="color:dodgerblue;">24/7 hour service</h3>
+					<p class="text-justify"> We are providing 24/7 hour service and pleased to help by staying beside you always.</p>
+				</article>
+			</div>
+
+		</div>
+
+
+
+          
+    </div>
+		
+	
+	
+	
+
+	
+ <?php include('footer.php'); ?>
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+	
+</body>
+</html>
