d40f17f09afa6a23b088105e4d86a9f64d5126a3
harsha-ys <68203453+harsha-ys@users.noreply.github.com>
2021-11-03 21:15:53 +0800

    patient

---
+++ echanel/patient/dashboard.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>mms-patient</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="../style.css">
+</head>
+<body>
+
+<?php
+
+
+
+?>
+	<?php include('uptomenu.php'); ?>
+
+
+	<!-- this is for donor registraton -->
+	<div class="dashboard" style="background-color:#fff;">
+		<h3 class="text-center" style="background-color:#272327;color: #fff;padding: 5px;">Welcome to Your Panel</h3>
+		<img src="img/patient.jpg" alt="logo.png img" class="img-responsive" style="margin-left:440px;width:300px;">
+		
+		
+			
+		
+	</div>
+	
+
+
+
+	
+</div><!--  containerFluid Ends -->
+
+<script src="js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
